ef449465e979a6d9acbbd92f5a68aab2766e1e51
mrazali <mohdfadzli.razali@sicpa.com>
2021-10-10 10:27:49 +0800

    - added model & enum - created plain CategoriesScreen

---
+++ lib/models/category.dart
@@ -0,0 +1,18 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+/// id : "c1"
+/// title : "Italian"
+/// color : Colors.orange
+
+class Category {
+  final String? id;
+  final String? title;
+  final Color? color;
+
+  const Category({
+    @required this.id,
+    @required this.title,
+    @required this.color,
+  });
+}
